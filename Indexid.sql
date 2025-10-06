eea26beafe29b118ee5b9817d3b2734af7f2d80b
Marek Lukk <mareklukk8@gmail.com>
2025-10-06 10:57:56 +0300

    Luuakse indeks veerule BaseRate tabelis, saab vaadata indekseid ja vajadusel kustutada selle indeksi.

---
+++ Indexid.sql
@@ -0,0 +1,7 @@
+SELECT * FROM DimEmployee
+CREATE INDEX IX_DimEmployee_BaseRate
+ON DimEmployee(BaseRate ASC)
+-- Kui soovid vaadata Indeksit
+EXEC sp_help DimEmployee;
+-- Kui soovid kustutada indeksit
+DROP INDEX DimEmployee.IX_DimEmployee_BaseRate
