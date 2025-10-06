39eb86884d2bd5e75db673480ce16dfc0d23afb8
Marek Lukk <mareklukk8@gmail.com>
2025-10-06 10:37:16 +0300

    Funktsioon tagastab töötaja nime ID järgi ja varjab oma koodi krüpteerimisega.

--- funktsioonid33.sql
+++ funktsioonid33.sql
@@ -7,3 +7,14 @@ return (select FirstName from DimEmployee Where EmployeeKey = @EmployeeKey)
 End
 
 exec sp_helptext fn_GetEmployeeNameById;
+-----alter funktsioon
+Alter Function fn_GetEmployeeNameById(@EmployeeKey int)
+
+Returns nvarchar(20)
+With Encryption 
+as
+begin
+Return (select FirstName from DimEmployee Where EmployeeKey = @EmployeeKey)
+End
+
+exec sp_helptext fn_GetEmployeeNameById;
