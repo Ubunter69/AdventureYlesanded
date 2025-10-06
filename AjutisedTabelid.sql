f3e3bdfc6ac0aa6cc54cc4d39d55ce268343e521
Marek Lukk <mareklukk8@gmail.com>
2025-10-06 11:52:25 +0300

    Protseduur loob globaalse ajutise tabeli kolme töötajaga, kuvab need ja tabel kustutatakse automaatselt pärast protseduuri lõppu

--- AjutisedTabelid.sql
+++ AjutisedTabelid.sql
@@ -1,8 +1,7 @@
 USE AdventureWorksDW2019;
 
 SELECT * FROM DimCustomer;
--- Allpool olevas SP-s luuakse ajutine tabel #PersonsDetails ja edastab andmeid ja 
--- lõhub ajutise tabeli automaatselt peale käsu lõpule jõudmist.
+--loon protseduuri et lokaalne ajutine tabel luua
 CREATE PROCEDURE spCreateLocalTempTable
 AS 
 BEGIN
@@ -14,3 +13,20 @@ SELECT * FROM #DimEmployee
 END
 -- Käivita funktsiooni
 EXEC spCreateLocalTempTable
+--loon protseduuri et lokaalne ajutine tabel luua
+CREATE PROCEDURE spCreateGlobalTempTable
+AS 
+BEGIN
+    CREATE TABLE ##DimEmployee(EmployeeKey INT, FirstName NVARCHAR(20))
+    INSERT INTO ##DimEmployee VALUES(1,'Mike')
+    INSERT INTO ##DimEmployee VALUES(2,'John')
+    INSERT INTO ##DimEmployee VALUES(3,'Todd')
+    SELECT * FROM ##DimEmployee
+END
+EXEC spCreateLocalTempTable;
+--Mitu kasutajat mitmes ühenduses saavad sama nimega lokaalse ajutise tabeli luua, 
+--aga globaalne peab olema unikaalse nimega. 
+--Kui uurid ajutist tabelit object exploreris, 
+--siis seal ei ole lõpus suvalisi numbreid tabeli nimes.
+
+
